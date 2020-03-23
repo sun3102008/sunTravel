c69cfff1a2d325cc9a122a7a7c207270f64a768c
kobe <sun3102008@163.com>
2020-03-23 21:59:30 +0800

    add axios

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -11,13 +11,28 @@ import HomeHeader from './childComponents/HomeHeader'
 import HomeSwiper from './childComponents/HomeSwiper'
 import HomeIcons from './childComponents/HomeIcons'
 
+import axios from 'axios'
+
 export default {
   name:'Home',
   components:{
     HomeHeader,
     HomeSwiper,
     HomeIcons
-  }
+  },
+  methods:{
+    getHomeInfo () {
+      axios.get('/api/index.json')
+        .then(this.getHomeInfoSucc)
+    },
+    getHomeInfoSucc (res) {
+      console.log(res)
+    }
+    
+  },
+  mounted() {
+    this.getHomeInfo()
+  },
 }
 </script>
 <style lang="">
